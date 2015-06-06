d4e1e3435f54cfbb5ae449b039f9345d40d8d5ed
csaki <cesar.arasaki.88@gmail.com>
2015-06-05 22:32:02 -0300

    FakkuDroid Beta V3  - Fragment

--- app/src/main/java/com/devsaki/fakkudroid/ContentListActivity.java
+++ app/src/main/java/com/devsaki/fakkudroid/ContentListActivity.java
@@ -1,17 +1,22 @@
 package com.devsaki.fakkudroid;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListAdapter;
@@ -29,119 +34,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class ContentListActivity extends ActionBarActivity {
+public class ContentListActivity extends FakkuDroidActivity {
 
     private static final String TAG = ContentListActivity.class.getName();
-    private FakkuDroidDB db;
-    private List<Content> contents;
-    private static String query = "";
-    private SharedPreferences sharedPreferences;
-    private int currentPage = 1;
+    private ContentListFragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_content_list);
-
-        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        db = new FakkuDroidDB(this);
-        ImageButton btnBrowser = (ImageButton) findViewById(R.id.btnBrowser);
-        btnBrowser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ContentListActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
-        ImageButton btnRefresh = (ImageButton) findViewById(R.id.btnRefresh);
-        btnRefresh.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                searchContent();
-            }
-        });
-        ImageButton btnDownloadManager = (ImageButton) findViewById(R.id.btnDownloadManager);
-        btnDownloadManager.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ContentListActivity.this, DownloadManagerActivity.class);
-                startActivity(intent);
-            }
-        });
-        ImageButton btnNext = (ImageButton) findViewById(R.id.btnNext);
-        btnNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int qtyPages = Integer.parseInt(sharedPreferences.getString(ConstantsPreferences.PREF_QUANTITY_PER_PAGE_LISTS, ConstantsPreferences.PREF_QUANTITY_PER_PAGE_DEFAULT + ""));
-                if (qtyPages <= 0) {
-                    Toast.makeText(ContentListActivity.this, R.string.not_limit_per_page, Toast.LENGTH_SHORT).show();
-                } else {
-                    currentPage++;
-                    searchContent();
-                }
-            }
-        });
-        ImageButton btnPrevius = (ImageButton) findViewById(R.id.btnPrevius);
-        btnPrevius.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int qtyPages = Integer.parseInt(sharedPreferences.getString(ConstantsPreferences.PREF_QUANTITY_PER_PAGE_LISTS, ConstantsPreferences.PREF_QUANTITY_PER_PAGE_DEFAULT + ""));
-                if (qtyPages <= 0) {
-                    Toast.makeText(ContentListActivity.this, R.string.not_limit_per_page, Toast.LENGTH_SHORT).show();
-                } else {
-                    if (currentPage > 1) {
-                        currentPage--;
-                        searchContent();
-                    } else {
-                        Toast.makeText(ContentListActivity.this, R.string.not_limit_per_page, Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }
-        });
-        String settingDir = sharedPreferences.getString(Constants.SETTINGS_FAKKUDROID_FOLDER, "");
-        boolean showMessageSupport = sharedPreferences.getBoolean(ConstantsPreferences.SHOW_MESSAGE_SUPPORT, true);
-        if (settingDir.isEmpty()) {
-            Intent intent = new Intent(this, SelectFolderActivity.class);
-            startActivity(intent);
-            finish();
-        } else {
-            searchContent();
-            if (showMessageSupport) {
-                Intent intent = new Intent(this, MessageSupportActivity.class);
-                startActivity(intent);
-            }
-        }
-    }
-
-    private void searchContent() {
-        int order = sharedPreferences.getInt(ConstantsPreferences.PREF_ORDER_CONTENT_LISTS, ConstantsPreferences.PREF_ORDER_CONTENT_BY_DATE);
-        int qtyPages = Integer.parseInt(sharedPreferences.getString(ConstantsPreferences.PREF_QUANTITY_PER_PAGE_LISTS, ConstantsPreferences.PREF_QUANTITY_PER_PAGE_DEFAULT + ""));
-        if (qtyPages > 0) {
-            contents = (List<Content>) db.selectContentByQuery(query, currentPage, qtyPages, order==ConstantsPreferences.PREF_ORDER_CONTENT_ALPHABETIC);
-        } else {
-            contents = (List<Content>) db.selectContentByQuery(query, order==ConstantsPreferences.PREF_ORDER_CONTENT_ALPHABETIC);
-        }
-        if (contents == null) {
-            contents = new ArrayList<>();
-        }
-        if (query.isEmpty())
-            setTitle(R.string.app_name);
-        else
-            setTitle(getResources().getString(R.string.title_activity_search).replace("@search", query));
-
-        ContentAdapter adapter = new ContentAdapter(this, contents);
-        setListAdapter(adapter);
-
-        Button btnPage = (Button) findViewById(R.id.btnPage);
-        btnPage.setText("" + currentPage);
+        fragment = new ContentListFragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_content_list, menu);
-
-
 
         // Associate searchable configuration with the SearchView
         SearchManager searchManager =
@@ -155,16 +64,15 @@ public class ContentListActivity extends ActionBarActivity {
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
-                query = s.trim();
-                currentPage = 1;
-                searchContent();
+                fragment.setQuery(s.trim());
+                fragment.searchContent();
                 return true;
             }
 
             @Override
             public boolean onQueryTextChange(String s) {
-                query = s.trim();
-                searchContent();
+                fragment.setQuery(s.trim());
+                fragment.searchContent();
                 return true;
             }
         });
@@ -187,30 +95,137 @@ public class ContentListActivity extends ActionBarActivity {
             SharedPreferences.Editor editor = sharedPreferences.edit();
             editor.putInt(ConstantsPreferences.PREF_ORDER_CONTENT_LISTS, ConstantsPreferences.PREF_ORDER_CONTENT_ALPHABETIC);
             editor.commit();
-            searchContent();
+            fragment.searchContent();
             return true;
         }else if (id == R.id.action_order_by_date) {
             SharedPreferences.Editor editor = sharedPreferences.edit();
             editor.putInt(ConstantsPreferences.PREF_ORDER_CONTENT_LISTS, ConstantsPreferences.PREF_ORDER_CONTENT_BY_DATE);
             editor.commit();
-            searchContent();
+            fragment.searchContent();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    private ListView mListView;
+    public static class ContentListFragment extends Fragment{
+        private static String query = "";
+        private int currentPage = 1;
+        private List<Content> contents;
 
-    private ListView getListView() {
-        if (mListView == null) {
-            mListView = (ListView) findViewById(R.id.list);
+        private FakkuDroidActivity getFakkuDroidActivity(){
+            return (FakkuDroidActivity) getActivity();
         }
-        return mListView;
-    }
 
-    private void setListAdapter(ListAdapter adapter) {
-        getListView().setAdapter(adapter);
-    }
+        public void setQuery(String query) {
+            ContentListFragment.query = query;
+            currentPage = 1;
+        }
 
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_content_list, container, false);
+            mListView = (ListView) rootView.findViewById(R.id.list);
+            btnPage = (Button) rootView.findViewById(R.id.btnPage);
+
+            ImageButton btnBrowser = (ImageButton) rootView.findViewById(R.id.btnBrowser);
+            btnBrowser.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getActivity(), MainActivity.class);
+                    startActivity(intent);
+                }
+            });
+            ImageButton btnRefresh = (ImageButton) rootView.findViewById(R.id.btnRefresh);
+            btnRefresh.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    searchContent();
+                }
+            });
+            ImageButton btnDownloadManager = (ImageButton) rootView.findViewById(R.id.btnDownloadManager);
+            btnDownloadManager.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getActivity(), DownloadManagerActivity.class);
+                    startActivity(intent);
+                }
+            });
+            ImageButton btnNext = (ImageButton) rootView.findViewById(R.id.btnNext);
+            btnNext.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int qtyPages = Integer.parseInt(getFakkuDroidActivity().sharedPreferences.getString(ConstantsPreferences.PREF_QUANTITY_PER_PAGE_LISTS, ConstantsPreferences.PREF_QUANTITY_PER_PAGE_DEFAULT + ""));
+                    if (qtyPages <= 0) {
+                        Toast.makeText(getActivity(), R.string.not_limit_per_page, Toast.LENGTH_SHORT).show();
+                    } else {
+                        currentPage++;
+                        searchContent();
+                    }
+                }
+            });
+            ImageButton btnPrevius = (ImageButton) rootView.findViewById(R.id.btnPrevius);
+            btnPrevius.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int qtyPages = Integer.parseInt(getFakkuDroidActivity().sharedPreferences.getString(ConstantsPreferences.PREF_QUANTITY_PER_PAGE_LISTS, ConstantsPreferences.PREF_QUANTITY_PER_PAGE_DEFAULT + ""));
+                    if (qtyPages <= 0) {
+                        Toast.makeText(getActivity(), R.string.not_limit_per_page, Toast.LENGTH_SHORT).show();
+                    } else {
+                        if (currentPage > 1) {
+                            currentPage--;
+                            searchContent();
+                        } else {
+                            Toast.makeText(getActivity(), R.string.not_limit_per_page, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            });
+            String settingDir = getFakkuDroidActivity().sharedPreferences.getString(Constants.SETTINGS_FAKKUDROID_FOLDER, "");
+            boolean showMessageSupport = getFakkuDroidActivity().sharedPreferences.getBoolean(ConstantsPreferences.SHOW_MESSAGE_SUPPORT, true);
+            if (settingDir.isEmpty()) {
+                Intent intent = new Intent(getActivity(), SelectFolderActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            } else {
+                searchContent();
+                if (showMessageSupport) {
+                    Intent intent = new Intent(getActivity(), MessageSupportActivity.class);
+                    startActivity(intent);
+                }
+            }
+            return rootView;
+        }
+
+        private void searchContent() {
+            int order = getFakkuDroidActivity().sharedPreferences.getInt(ConstantsPreferences.PREF_ORDER_CONTENT_LISTS, ConstantsPreferences.PREF_ORDER_CONTENT_BY_DATE);
+            int qtyPages = Integer.parseInt(getFakkuDroidActivity().sharedPreferences.getString(ConstantsPreferences.PREF_QUANTITY_PER_PAGE_LISTS, ConstantsPreferences.PREF_QUANTITY_PER_PAGE_DEFAULT + ""));
+            if (qtyPages > 0) {
+                contents = (List<Content>) getFakkuDroidActivity().db.selectContentByQuery(query, currentPage, qtyPages, order==ConstantsPreferences.PREF_ORDER_CONTENT_ALPHABETIC);
+            } else {
+                contents = (List<Content>) getFakkuDroidActivity().db.selectContentByQuery(query, order==ConstantsPreferences.PREF_ORDER_CONTENT_ALPHABETIC);
+            }
+            if (contents == null) {
+                contents = new ArrayList<>();
+            }
+            if (query.isEmpty())
+                getActivity().setTitle(R.string.app_name);
+            else
+                getActivity().setTitle(getResources().getString(R.string.title_activity_search).replace("@search", query));
+
+            ContentAdapter adapter = new ContentAdapter(getActivity(), contents);
+            setListAdapter(adapter);
+
+
+            btnPage.setText("" + currentPage);
+        }
+
+        private ListView mListView;
+        private Button btnPage;
+
+        private void setListAdapter(ListAdapter adapter) {
+            mListView.setAdapter(adapter);
+        }
+    }
 }
--- app/src/main/java/com/devsaki/fakkudroid/FakkuDroidActivity.java
+++ app/src/main/java/com/devsaki/fakkudroid/FakkuDroidActivity.java
@@ -1,11 +1,89 @@
 package com.devsaki.fakkudroid;
 
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.devsaki.fakkudroid.database.FakkuDroidDB;
 
 /**
  * Created by DevSaki on 04/06/2015.
  */
-public class FakkuDroidActivity extends ActionBarActivity {
+public abstract class FakkuDroidActivity extends ActionBarActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    protected FakkuDroidDB db;
+    protected SharedPreferences sharedPreferences;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fakkudroid);
 
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+        ) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        db = new FakkuDroidDB(this);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void navigationDrawerWebView(View view){
+
+    }
+
+    public void navigationDrawerPreferences(View view){
+
+    }
 }
